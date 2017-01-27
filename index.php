90cbec5b2b92f780f533be782c1ec29f08ace9aa
akatot <bam.akatot@gmail.com>
2017-01-28 00:18:15 +0900

    discordbot-test

--- index.php
+++ index.php
@@ -1,5 +1,26 @@
 <?php
 
+//echo "hello world";
+
+include __DIR__.'/vendor/autoload.php';
+
+use Discord\Discord;
+
+$discord = new Discord([
+    'token' => 'bot-token',
+]);
+
+$discord->on('ready', function ($discord) {
+    echo "hello world", PHP_EOL;
+
+    // Listen for messages.
+    $discord->on('message', function ($message, $discord) {
+        echo "{$message->author->username}: {$message->content}",PHP_EOL;
+    });
+});
+
+$discord->run();
+
 echo "hello world";
 
 ?>
